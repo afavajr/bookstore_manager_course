7b2159b65adb701eca3a088c76b68954497992f4
afavajr <afavajrbr80@gmail.com>
2021-07-13 09:44:10 -0400

    .

--- src/main/java/com/antoniofava/bookstoremanager/entity/Book.java
+++ src/main/java/com/antoniofava/bookstoremanager/entity/Book.java
@@ -1,14 +1,32 @@
-package com.antoniofava.bookstoremanager.entity;
+package com.rodrigopeleias.bookstoremanager.entity;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
-    @Column(nullable = false, unique = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
@@ -23,9 +41,7 @@ public class Book {
     @Column(name = "publisher_name", nullable = false, unique = true)
     private String publisherName;
 
-    @ManyToOne(fetch = FetchType.LAZY cascade = {CascadeType.PERSIST, CascadeType.Merge, CascadeType.REMOVE}
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     @JoinColumn(name = "author_id")
     private Author author;
-
-
 }
