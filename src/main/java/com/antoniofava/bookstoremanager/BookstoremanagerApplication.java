5525d0d98e28806899c81e845092c4b769c104ee
afavajr <afavajrbr80@gmail.com>
2021-07-13 11:05:26 -0400

    teste

--- src/main/java/com/antoniofava/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/antoniofava/bookstoremanager/BookstoremanagerApplication.java
@@ -3,7 +3,7 @@ package com.antoniofava.bookstoremanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class BookstoremanagerApplication {
 
 	public static void main(String[] args) {
